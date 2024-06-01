eeaa43e36d754afaa64b45e6526c21f895d13c6f
Randy Brown <thatrandybrown@gmail.com>
2024-05-31 20:18:39 -0500

    pass an arg to the script

--- bootstrap.sh
+++ bootstrap.sh
@@ -17,6 +17,8 @@ echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> ~/.zprofile
 eval "$(/opt/homebrew/bin/brew shellenv)"
 brew doctor
 
+echo $1
+
 # once docker is installed, everything should probably be managed by ansible
 # anything added below this line should be migrated to ansible
 
